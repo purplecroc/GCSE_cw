0de4bc20d86077e7d9e353f8827c7d6689211230
Ewan Spencer <ewan@thespencers.me.uk>
2024-01-23 10:46:56 +0000

    Add quiz logic

--- task_3.py
+++ task_3.py
@@ -1,5 +1,18 @@
 USER_PASSWORD_FILE = "passwordfile.txt"
 
+QUESTIONS = {
+    "Question 1": [("Answer 1.1", True), ("Answer 1.2", False), ("Answer 1.3", False), ("Answer 1.4", False)],
+    "Question 2": [("Answer 1.1", True), ("Answer 1.2", False), ("Answer 1.3", False), ("Answer 1.4", False)],
+    "Question 3": [("Answer 1.1", True), ("Answer 1.2", False), ("Answer 1.3", False), ("Answer 1.4", False)],
+    # "Question 4": [("Answer 1.1", True), ("Answer 1.2", False), ("Answer 1.3", False), ("Answer 1.4", False)],
+    # "Question 5": [("Answer 1.1", True), ("Answer 1.2", False), ("Answer 1.3", False), ("Answer 1.4", False)],
+    # "Question 6": [("Answer 1.1", True), ("Answer 1.2", False), ("Answer 1.3", False), ("Answer 1.4", False)],
+    # "Question 7": [("Answer 1.1", True), ("Answer 1.2", False), ("Answer 1.3", False), ("Answer 1.4", False)],
+    # "Question 8": [("Answer 1.1", True), ("Answer 1.2", False), ("Answer 1.3", False), ("Answer 1.4", False)],
+    # "Question 9": [("Answer 1.1", True), ("Answer 1.2", False), ("Answer 1.3", False), ("Answer 1.4", False)],
+    # "Question 10": [("Answer 1.1", True), ("Answer 1.2", False), ("Answer 1.3", False), ("Answer 1.4", False)],
+}
+
 # SUBROUTINE store_login_details(username, password)​
 #     file <- openWrite("passwordfile.txt")​
 #     file.wrtireLine(username + ":" + password)​
@@ -68,6 +81,7 @@ def login():
     if command == "NEW":
         (username, password) = get_user_and_password()
         store_login_details(username, password)
+        return username
     elif command == "EXISTING":
         while True:
             (username, password) = get_user_and_password()
@@ -76,7 +90,84 @@ def login():
             else:
                 print("invalid username or password")
 
-username = None
-while username == None:
-    username = login()
-print(username)
+# SUBROUTINE ask_question(question, answers)​
+#  PRINT(question)​
+#  FOR answer in answers​
+#   PRINT(answer)​
+#  ENDFOR​
+# ENDSUBROUTINE
+
+def ask_question(question, answers):
+    print("Q:",question)
+    for index, (answer, correct) in enumerate(answers):
+        print("A " + str(index + 1) + ":", answer)
+    
+        
+
+# SUBROUTINE play_again()​
+#  WHILE TRUE​
+#   PRINT("play again (y/n)?")​
+#   IF USERINPUT = "y"​
+#    RETURN TRUE​
+#   ELSEIF USERINPUT = "n"​
+#    RETURN FALSE​
+#   PRINT("Invalid input")​
+#  ENDWHILE​
+# ENDSUBROUTINE
+
+def play_again():
+    while True:
+        response = input("play again (y/n)? ").upper()
+        if response == "Y":
+            return True
+        elif response == "N":
+            return False
+        print("invalid input")
+
+# SUBROUTINE quiz()​
+#  questions -> dictionary of questions to list of answers​
+#  playing_quiz = TRUE​
+#  WHILE playing_quiz​
+#   FOR question and answers in questions ​
+#    ask_question(question, answers)​
+#   ENDFOR​
+#   playing_quiz = play_again()​
+#  ENDWHILE​
+#  PRINT("Thank you for playing")
+# ENDSUBROUTINE
+
+def quiz():
+    playing_quiz = True
+    score = 0
+    while playing_quiz:
+        for question, answers in QUESTIONS.items():
+            ask_question(question, answers)
+            player_answer = None
+            while player_answer == None:
+                try:
+                    i = int(input("answer (1, 2, 3, 4)? "))
+                    if i in range(1,5):
+                        player_answer = i
+                except:
+                    i = None
+            (_, correct) = answers[player_answer-1]
+            if correct == True:
+                score += 1
+        print("you got", score, "correct")
+        playing_quiz = play_again()
+    print("thank you for playing")
+    return score
+
+# ask_question("blah?", ["b;ah", "bleh", "bloo"])
+
+# username = None
+# while username == None:
+#     username = login()
+# print(username)
+
+# def play_quiz():
+#     username = login()
+#     score = quiz()
+    
+
+quiz()
